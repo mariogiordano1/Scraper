3d59023c6f4a89339d327e3cb1a765b372a9e006
PatrickClark <patrickclark@outlook.it>
2021-06-01 12:03:01 +0200

    change token

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "1865758524:AAFJQcJSueumMYnzhgrN8EnKx1txxLMmsxw"
+bot_token = "1843813060:AAHm6B2-i8T2B-FThx4YoaWGTjy669dP060"
 bot_user_name = "PippettinoBot"
 URL = "https://enigmatic-sea-00849.herokuapp.com/"
